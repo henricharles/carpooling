1b0c7fa112147669c327316c227d473075c12f7b
charles <2kkhenricharles@gmail.com>
2016-12-19 22:54:46 +0530

    pom modifiction

--- src/dao/DbConnection.java
+++ src/dao/DbConnection.java
@@ -3,26 +3,45 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
 
 public class DbConnection {
+	Connection con;
+	 Statement stmt;
+	 ResultSet rs;
  public void connect()
  {
 	 try{
 	 Class.forName("com.mysql.jdbc.Driver");  
-	 Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/carpoolingdb","root","");  
+	 con=DriverManager.getConnection("jdbc:mysql://localhost:3306/carpoolingdb","root","");  
 	 Statement stmt=con.createStatement();  
-	 ResultSet rs=stmt.executeQuery("select * from like");  
+	// ResultSet rs=stmt.executeQuery("select * from likes");  
 	 System.out.println("hello");
 	 }
 	 catch(Exception e)
 	 {
-		 
+		 e.printStackTrace();
 	 }
  }
- public static void main(String s)
- {
-	 DbConnection d=new  DbConnection() ;
-	 d.connect();
- }
+
+
+
+public void createUser()
+{
+	String query1="insert into users VALUES(henri,1,Iowa,fairfield,4th ,52556,1988,ch@gam.com,'hello')";
+	this.connect();
+	try {
+		stmt.executeQuery(query1);
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+
+	
+	
+	
+}
 }
